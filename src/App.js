8466ff272ba22ae5f0d6f78cdf1f4142a16ab96b
anilkumar <anil.amrutha44@gmail.com>
2021-05-14 21:12:44 +0530

    added set intervel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
+import React from 'react';
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { message: "" };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        message: "Hello World!, after 5 seconds"
+      })
+    }, 5000);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{ this.state.message }</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
